f6251ea14a2b47daa85823e14facf4c1e7960244
yogendrasinghrawat <git.yrawat@gmail.com>
2022-05-27 19:27:58 +0530

    Completed till 26th May

--- src/test/java/FreshRestAssured/TestingRA/Testcases.java
+++ src/test/java/FreshRestAssured/TestingRA/Testcases.java
@@ -1,11 +1,11 @@
 package FreshRestAssured.TestingRA;
 
+import org.hamcrest.core.IsEqual;
 import org.json.simple.JSONObject;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.hamcrest.core.IsEqual;
-import static org.hamcrest.Matchers.equalTo;
+
 import com.relevantcodes.extentreports.LogStatus;
 
 import io.restassured.RestAssured;
@@ -16,101 +16,91 @@ import static io.restassured.RestAssured.given;
 
 public class Testcases {
 	
+	
 	ResponseSpecification spec = null;
 	@BeforeClass
 	public void setSpecification() {
 		spec = RestAssured.expect();
 		spec.contentType(ContentType.JSON);
 		spec.statusCode(200);
-		spec.statusLine("HTTP/1.1 200 OK");		
-		ExtentReportManager.createReport();
+		spec.statusLine("HTTP/1.1 200 OK");
+		
+		ExtentReportMAnager.createReport();
 	}
 	
 	@Test
 	public void getListOfUsers() {
 		
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("getListOfUsers", "Gets the list of Users");
+		ExtentReportMAnager.test = ExtentReportMAnager.reports.startTest("getListOfUsers", "Gets the list of Users");
 		try {
-			
 			RestAssured.baseURI = "https://dummy.restapiexample.com/";
-			ExtentReportManager.test.log(LogStatus.INFO, "Configured the BASE URL", RestAssured.baseURI);
+			ExtentReportMAnager.test.log(LogStatus.INFO, "Configured the base URL", RestAssured.baseURI);
 			//String message = given().when().get("api/v1/employees").then().spec(spec).extract().path("message");
-			ExtentReportManager.test.log(LogStatus.INFO, "Make a GET API call", "api/v1/employees");
-			//ExtentReportManager.test.log(LogStatus.INFO, "Extract the response message", message);
+			ExtentReportMAnager.test.log(LogStatus.INFO, "Make a GET API call", "api/v1/employees");
+			//ExtentReportMAnager.test.log(LogStatus.INFO, "Extract the response message", message);
 			//System.out.println(message);
-			ExtentReportManager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! All records has been fetched.");
-			//ExtentReportManager.test.log(LogStatus.INFO, "Actual Message -> ", message);
-			given().contentType(ContentType.JSON).when().get("api/v1/employees").then().body("message", equalTo("Successfully! All records has been fetched.")) ;
-			
-			ExtentReportManager.test.log(LogStatus.PASS, "Result", "PASS");
-			
+			ExtentReportMAnager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! All records has been fetched.");
+			//ExtentReportMAnager.test.log(LogStatus.INFO, "Actual Message -> ", message);
+			given().when().get("api/v1/employees").then().assertThat().body("message", IsEqual.equalTo("Successfully! All records has been fetched."));
 		}
-		
-		catch(Exception ex){
-			
-			ExtentReportManager.test.log(LogStatus.ERROR, "Exception Occured", ex.getMessage());
-			ExtentReportManager.test.log(LogStatus.FAIL , "TC Failed", ex.getMessage());
-			
+		catch(Exception ex)
+		{
+			ExtentReportMAnager.test.log(LogStatus.ERROR, "Exception Occured -> ", ex.getMessage());
+			ExtentReportMAnager.test.log(LogStatus.FAIL, "TC FAiled",ex.getMessage());
 		}
-		
-		
-		/*if (message.equals("Successfully! All records has been fetched.")) {
+		/*
+		if (message.equals("Successfully! All records has been fetched.")) {
 			System.out.println("Test case passed");
 		}
 		else
 		{
 			System.out.println("Test case failed");
-		}*/
+		}
+		*/
 	}
-	
 	
 	@Test
 	@SuppressWarnings("unchecked")
 	public void CreateUser() {
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("CreateUser", "Create An User");
+		ExtentReportMAnager.test = ExtentReportMAnager.reports.startTest("CreateUser", "Create a User");
 		JSONObject params = new JSONObject();
 		params.put("name", "Jacob");
 		params.put("age", "20");
 		params.put("salary", "200000");
-		ExtentReportManager.test.log(LogStatus.INFO, "Configured the JSON request body ", params.toJSONString());
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Configured the JSON request body", params.toJSONString());
 		RestAssured.baseURI = "https://dummy.restapiexample.com/";
-		ExtentReportManager.test.log(LogStatus.INFO, "Configured the BASE URL", RestAssured.baseURI);
-		ExtentReportManager.test.log(LogStatus.INFO, "Make a POST API call", "api/v1/create");
-		Response response = given().when().header("Content-Type", "application/json").body(params.toJSONString()).post("api/v1/create");
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Configured the base URL", RestAssured.baseURI);
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Make a POST API call", "api/v1/create");
+		Response response = given().header("Content-Type", "application/json").body(params.toJSONString()).post("api/v1/create");
+		System.out.println("The status received: " + response.asString());
 		
-		ExtentReportManager.test.log(LogStatus.INFO, "Expected Status Code ", "200");
-		ExtentReportManager.test.log(LogStatus.INFO, "Actual Status Code ", Integer.toString(response.statusCode()));
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Expected Status code", "200");
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Actual Status Code", Integer.toString(response.statusCode()));
 		
-
-
 	}
-	
 	
 	@Test
 	public void getSpecificUser() {
 		//ResponseSpecification spec2 = RestAssured.expect();
 		//spec2.contentType(ContentType.JSON);
 		//spec2.statusCode(200);
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("getSpecificUser", "Fetch record of One user");
+		ExtentReportMAnager.test = ExtentReportMAnager.reports.startTest("getSpecificUser", "Fetch record of one user");
 		RestAssured.baseURI = "https://dummy.restapiexample.com/";
-		ExtentReportManager.test.log(LogStatus.INFO, "Configured the BASE URL", RestAssured.baseURI);
-		ExtentReportManager.test.log(LogStatus.INFO, "Make a GET API call", "api/v1/employee/<id>");
-		ExtentReportManager.test.log(LogStatus.INFO, "Employee ID ", "22");
-		ExtentReportManager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! Record has been fetched.");
-		try {
-		given().when().get("api/v1/employee/22").then().assertThat().body("message", IsEqual.equalTo("Successfully! Record has been fetched."));
-		//given().when().get("api/v1/employees").then().assertThat().body("message", IsEqual.equalTo("Successfully! All records has been fetched.")) ;
-		//ExtentReportManager.test.log(LogStatus.INFO, "Actual Result", message);
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Configured the base URL", RestAssured.baseURI);
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Make a GET api call", "api/v1/employee/<id>");
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Employee ID", "22");
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! Record has been fetched");
+		try
+		{
+			given().when().get("api/v1/employee/22").then().assertThat().body("message", IsEqual.equalTo("Successfully! Record has been fetched"));
 		}
-		
-		catch(Exception ex){
-			
-			ExtentReportManager.test.log(LogStatus.ERROR, "Exception Occured", ex.getMessage());
-			ExtentReportManager.test.log(LogStatus.FAIL , "TC Failed", ex.getMessage());
-			
+		catch(Exception ex)
+		{
+			ExtentReportMAnager.test.log(LogStatus.ERROR, "Exception Occured", ex.getMessage());
+			ExtentReportMAnager.test.log(LogStatus.FAIL, "TC Failed", ex.getMessage());
 		}
-		
-		
+		//String message = given().when().get("api/v1/employee/5777").then().spec(spec).extract().path("message");
+		//System.out.println(message);
 		
 	}
 	
@@ -118,62 +108,43 @@ public class Testcases {
 	@Test
 	public void DeleteUser() {
 		
-		
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("DeleteSpecificUser", "Delete An User record");
+		ExtentReportMAnager.test = ExtentReportMAnager.reports.startTest("deleteSpecificUser", "Delete a user record");
 		
 		RestAssured.baseURI = "https://dummy.restapiexample.com/";
-		ExtentReportManager.test.log(LogStatus.INFO, "Configured the BASE URL", RestAssured.baseURI);
-		ExtentReportManager.test.log(LogStatus.INFO, "Make a DELETE API call", "/api/v1/delete/<id>");
-		ExtentReportManager.test.log(LogStatus.INFO, "Employee ID ", "2");
-		ExtentReportManager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! Record has been deleted");
-		try {
-		given().delete("/api/v1/delete/2").then().assertThat().body("message", IsEqual.equalTo("Successfully! Record has been deleted"));
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Configured the BASE url", RestAssured.baseURI);
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Make a DELETE api call", "/api/v1/delete/<id>");
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Employee ID", "2");
+		ExtentReportMAnager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! Record has been deleted");
+		try
+		{
+			given().delete("/api/v1/delete/2").then().assertThat().body("message", IsEqual.equalTo("Successfully! Record has been deleted"));
 		}
-		
 		catch(Exception ex){
 			
-			ExtentReportManager.test.log(LogStatus.ERROR, "Exception Occured", ex.getMessage());
-			ExtentReportManager.test.log(LogStatus.FAIL , "TC Failed", ex.getMessage());
+			ExtentReportMAnager.test.log(LogStatus.ERROR, "Exception Occured", ex.getMessage());
+			ExtentReportMAnager.test.log(LogStatus.FAIL, "TC Failed", ex.getMessage());
+			
 			
 		}
-		
-		
+		Response response = given().delete("/api/v1/delete/2");
+		System.out.println("The status received: " + response.asString());
 
 	}
-	
 	
 	@Test
 	public void UpdateUser() {
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("UpdateUser", "Update An User record");
-
+		
 		RestAssured.baseURI = "https://dummy.restapiexample.com/";
-		
-		ExtentReportManager.test.log(LogStatus.INFO, "Configured the BASE URL", RestAssured.baseURI);
-		ExtentReportManager.test.log(LogStatus.INFO, "Make a PUT API call", "/api/v1/update/<id>");
-		ExtentReportManager.test.log(LogStatus.INFO, "Employee ID ", "21");
-		ExtentReportManager.test.log(LogStatus.INFO, "Expected Message -> ", "Successfully! Record has been updated.");
-		try {
-		given().put("/api/v1/update/21").then().assertThat().body("message", IsEqual.equalTo("Successfully! Record has been updated."));
-		}
-		
-		catch(Exception ex){
-			
-			ExtentReportManager.test.log(LogStatus.ERROR, "Exception Occured", ex.getMessage());
-			ExtentReportManager.test.log(LogStatus.FAIL , "TC Failed", ex.getMessage());
-			
-		}
-		
+		Response response = given().put("/api/v1/update/21");
+		System.out.println("The status received: " + response.asString());
 		
 	}
-	
 	
 	@AfterClass
-	public void endReport() {
-		
-		ExtentReportManager.reports.flush();
+	public void endReport()
+	{
+		ExtentReportMAnager.reports.flush();
 	}
-	
-	
 
 	
 
